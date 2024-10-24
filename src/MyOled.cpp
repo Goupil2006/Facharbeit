78680ebe6b27e7e0d91b42921679c2faab17beab
Goupil2000 <60356885+Goupil2006@users.noreply.github.com>
2024-10-24 20:54:13 +0200

    Final bevor Abgabe Anleitung

--- src/MyOled.cpp
+++ src/MyOled.cpp
@@ -1,75 +1,32 @@
 // Importiern der Librarys
 #include <Arduino.h>
-#include <U8g2lib.h>
+#include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 
 // Klasse definieren
 class MyOled
 {
 private:
-    U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2;
+    LiquidCrystal_I2C lcd;
 
 public:
     // Konstruktor
-    MyOled() : u8g2(U8G2_SH1106_128X64_NONAME_F_HW_I2C(U8G2_R0, /* reset=*/U8X8_PIN_NONE))
+    MyOled() : lcd(0x27, 20, 4)
     {
     }
 
     // Methode um das Oled zu initialisieren
     void init()
     {
-        u8g2.begin();
+        lcd.init();
     }
 
     // Methode um Text auf das Oled zu schreiben
-    void drawtexte(int x, int y, const char *text)
+    void drawtexte(int x, int y, String text)
     {
-        u8g2.setFont(u8g2_font_8x13_tf);
-        u8g2.setCursor(x, y);
-        u8g2.print(text);
-        update();
-    }
-
-    // Methode um ein Rechteck auf das Oled zu zeichnen (wird momentan noch nicht verwendet)
-    void drawRectangle(int x, int y, int width, int height)
-    {
-        u8g2.drawFrame(x, y, width, height);
-    }
-
-    // Methode um das Oled zu löschen
-    void clear() { u8g2.clear(); }
-
-    // Methode um den Buffer zu löschen
-    void clearBuffer()
-    {
-        u8g2.clearBuffer();
-    }
-
-    // Methode um ein Tipp auf das Oled zu zeichnen
-    void drawTip(int x, int y, const char *tip)
-    {
-        // Beispiel icon
-        const unsigned char light_bulb_bits[] = {
-            0x3c, 0x7e, 0x7e, 0x7e, 0x7e, 0x3c, 0x18, 0x18};
-
-        const int light_bulb_width = 10;
-        const int light_bulb_height = 10;
-
-        // Icon zeichnen
-        u8g2.drawXBMP(x, y, light_bulb_width, light_bulb_height, light_bulb_bits);
-
-        // Text zeichnen
-        u8g2.setFont(u8g2_font_8x13_tf);
-        u8g2.setCursor(x + light_bulb_width + 5, y + 10);
-        u8g2.print(tip);
-
-        // Buffer senden
-        update();
-    }
-
-    // Methode um den Buffer zu senden
-    void update()
-    {
-        u8g2.sendBuffer();
+        lcd.backlight();
+        lcd.clear();
+        lcd.setCursor(3, 0);
+        lcd.print(text);
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 // Importieren der Librarys
 #include <Arduino.h>
-#include <U8g2lib.h>
-#include <Wire.h>
+#include <Adafruit_BMP085.h>
+#include <SoftwareWire.h>
 
 // Importieren der Klassen
 #include "MySwitch.cpp"
@@ -12,116 +12,361 @@
 // IC2 Adresse definieren
 #define I2C_ADDRESS 0x3C
 
+Servo s1;
+Servo s2;
+Servo s3;
+Servo s4;
+Servo s5;
+
+SoftwareWire myWire1(37, 35); // SDA = 37, SCL = 35
+SoftwareWire myWire2(41, 39); // SDA = 41, SCL = 39
+SoftwareWire myWire3(45, 43); // SDA = 45, SCL = 43
+SoftwareWire myWire4(49, 47); // SDA = 49, SCL = 47
+SoftwareWire myWire5(53, 51); // SDA = 53, SCL = 51
+
+Adafruit_BMP085 bmp1(&myWire1);
+Adafruit_BMP085 bmp2(&myWire2);
+Adafruit_BMP085 bmp3(&myWire3);
+Adafruit_BMP085 bmp4(&myWire4);
+Adafruit_BMP085 bmp5(&myWire5);
+
+double pressure1;
+double pressure2;
+double pressure3;
+double pressure4;
+double pressure5;
+
 // Variablen definieren
 int anzahlPusten = 0;
-bool start = false;
+bool checking = false;
 int Timeout = 0;
+int stage = 0;
+double druck = 0;
 
 // Klassen für Komponenten definieren
 // MyAmpel ampel = MyAmpel();
-MyServo servo1 = MyServo(5, 0, 5);
+// MyServo servo1 = MyServo(5, 0, 5);
 MyOled oled1 = MyOled();
 MySwitch mySwitch = MySwitch(10);
 
+String currentdisplay = "";
+
+const int SERVO1 = 2;
+const int SERVO2 = 3;
+const int SERVO3 = 4;
+const int SERVO4 = 5;
+const int SERVO5 = 6;
+const int BUTTON = 22;
+const int Gyro = A13;
+
+const int correctConnections[5] = {bmp1, bmp2, bmp3, bmp4, bmp5};
+
+void start()
+{
+  s1.write(90);
+  delay(200);
+
+  s2.write(90);
+  delay(200);
+
+  s3.write(90);
+  delay(200);
+
+  s4.write(100);
+  delay(200);
+
+  s5.write(85);
+  delay(200);
+
+  s1.write(0);
+  oled1.drawtexte(0, 30, "An");
+  // Alle Komponenten und Variablen auf den Start der Überprüfung setzen
+  // ampel.turnOnGreen();
+  // servo1.reset();
+  mySwitch.on();
+  anzahlPusten = 0;
+  stage = 0;
+  checking = true;
+}
+
+void checkSuccess()
+{
+  // Erkannte luftströme erhöhen
+  stage++;
+
+  // move next servo
+  switch (stage)
+  {
+  case 1:
+    s1.write(90);
+    delay(1000);
+    s2.write(0);
+    delay(500);
+    break;
+  case 2:
+    s2.write(90);
+    delay(1000);
+    s3.write(0);
+    delay(500);
+  case 3:
+    s3.write(100);
+    delay(1000);
+    s4.write(0);
+    delay(500);
+  case 4:
+    s4.write(85);
+    delay(1000);
+    s5.write(0);
+    delay(500);
+  }
+
+  anzahlPusten++;
+
+  // Die Anzahl der Pusten auf dem Oled anzeigen
+  switch (stage)
+  {
+  case 1:
+    oled1.drawtexte(75, 40, "Anzahl: 1");
+    break;
+  case 2:
+    oled1.drawtexte(75, 40, "Anzahl: 2");
+    break;
+  case 3:
+    oled1.drawtexte(75, 40, "Anzahl: 3");
+    break;
+  case 4:
+    oled1.drawtexte(75, 40, "Anzahl: 4");
+    break;
+  case 5:
+    oled1.drawtexte(75, 40, "Anzahl: 5");
+    break;
+  default:
+    break;
+  }
+
+  // Den Servo um einen Schritt weiter geben
+  // servo1.addRotation(1);
+  delay(1000);
+
+  Timeout = 0;
+
+  // Wenn 5 Luftströme erkannt wurden
+  if (stage == 5)
+  {
+    s5.write(80);
+    // Alles Ausschalten und zurücksetzen
+    mySwitch.off();
+    {
+      // Nutzer über display mitteilen, das er es geschafft hat
+      oled1.drawtexte(0, 30, "Geschafft");
+      // servo1.reset();
+    }
+    delay(5000);
+    Timeout = 0;
+    anzahlPusten = 0;
+    checking = false;
+  }
+}
+
+void stopCheck()
+{
+  // Alles Ausschalten und zurücksetzen
+  mySwitch.off();
+  // Nutzer über display mitteilen, das er es nicht geschafft hat
+  oled1.drawtexte(0, 30, "Nicht Geschafft");
+  delay(1000);
+  switch (stage)
+  {
+  case 1:
+    oled1.drawtexte(75, 40, "Anzahl: 1");
+    break;
+  case 2:
+    oled1.drawtexte(75, 40, "Anzahl: 2");
+    break;
+  case 3:
+    oled1.drawtexte(75, 40, "Anzahl: 3");
+    break;
+  case 4:
+    oled1.drawtexte(75, 40, "Anzahl: 4");
+    break;
+  case 5:
+    oled1.drawtexte(75, 40, "Anzahl: 5");
+    break;
+  default:
+    break;
+  }
+  // servo1.reset();
+  delay(1000);
+  Timeout = 0;
+  anzahlPusten = 0;
+  checking = false;
+}
+
+bool isStartCase()
+{
+  return analogRead(Gyro) > 500 && !checking;
+}
+
+bool isAirflowAndChecking()
+{ // digitalRead(correctConnections[stage]) == HIGH
+  if (checking)
+  {
+    if (correctConnections[stage].readPressure() + 500 > pressure1)
+    {
+      return true;
+    }
+    else
+    {
+      delay(500);
+      if (correctConnections[stage].readPressure() + 500 > pressure1)
+      {
+        return true;
+      }
+      else
+      {
+        delay(500);
+        if (correctConnections[stage].readPressure() + 500 > pressure1)
+        {
+          return true;
+        }
+        else
+        {
+          delay(500);
+        }
+      }
+    }
+  }
+  return correctConnections[stage].readPressure() + 500 > pressure1 && checking;
+}
+
+bool isNoAirflow()
+{
+  return Timeout > 10;
+}
+
+void checkLoop()
+{
+  if (isStartCase())
+  {
+    s1.write(0);
+    start();
+    currentdisplay = "Click";
+    if (currentdisplay != "Click")
+    {
+      oled1.drawtexte(0, 30, "Click");
+    }
+  }
+  else
+  {
+    currentdisplay = "Ready";
+    if (currentdisplay != "Ready")
+    {
+      oled1.drawtexte(0, 30, "Ready");
+    }
+  }
+
+  // // Wenn die Überprüfung noch nicht gestartet ist
+  if (!checking)
+  {
+    //   // Die Ampel auf Rot setzen
+    //   // ampel.turnOnRed();
+    Timeout = 0;
+  }
+
+  // // Wenn während der Überprüfung Airflow erkannt wird
+  if (isAirflowAndChecking())
+  {
+    checkSuccess();
+  }
+  else
+  {
+    Timeout++;
+    delay(50);
+  }
+
+  // // Falls für 0,5 Sekunden kein Luftstrom erkannt wird
+  if (isNoAirflow())
+  {
+    stopCheck();
+  }
+
+  // // Wenn die Überprüfung gestartet wurde und kein Luftstrom erkannt wird
+}
+
 void setup()
 {
+  // Luftdurcksensoren Initialisieren
+  // Den Oled initialisieren
+  oled1.init();
+  oled1.drawtexte(0, 30, "TEST");
+
+  // Servos für Ventile
+  s1.attach(SERVO1, 500, 1500);
+  s2.attach(SERVO2, 475, 1500);
+  s3.attach(SERVO3, 900, 1500);
+  s4.attach(SERVO4, 900, 1800);
+  s5.attach(SERVO5, 475, 1500);
+
+  // Überprüfen der Servos
+  s1.write(0);
+  delay(1000);
+  s1.write(90);
+  delay(1000);
+
+  s2.write(0);
+  delay(1000);
+  s2.write(90);
+  delay(1000);
+
+  s3.write(0);
+  delay(1000);
+  s3.write(90);
+  delay(1000);
+
+  s4.write(0);
+  delay(1000);
+  s4.write(100);
+  delay(1000);
+
+  s5.write(0);
+  delay(1000);
+  s5.write(85);
+  delay(1000);
+
   // Airflow Sensor zu "input" setzen
-  pinMode(32, INPUT); // Aktueller
+  pinMode(AIRFLOW1, INPUT);
+  pinMode(AIRFLOW2, INPUT);
+  pinMode(AIRFLOW3, INPUT);
+  pinMode(AIRFLOW4, INPUT);
+  pinMode(AIRFLOW5, INPUT);
 
   // Den Knopf zu "input" setzen
-  pinMode(52, INPUT);
+  pinMode(BUTTON, INPUT);
+
+  // BMPS Inizaliesieren
+  myWire1.begin();
+  myWire2.begin();
+  myWire3.begin();
+  myWire4.begin();
+  myWire5.begin();
+
+  pressure1 = bmp1.readPressure();
+  pressure2 = bmp2.readPressure();
+  pressure3 = bmp3.readPressure();
+  pressure4 = bmp4.readPressure();
+  pressure5 = bmp5.readPressure();
 
   // Die Ampel initialisieren auf die Pins 1, 2 und 3
   // ampel.init(1, 2, 3);
 
   // Den Servo initialisieren auf Pin 5
-  servo1.init(5);
-
-  // Den Oled initialisieren
-  oled1.init();
+  // ervo1.init(5);
 
   // Den Switch initialisieren um die Steckdose zu schalten
-  mySwitch.init(10);
+  mySwitch.init(11);
 }
 
 void loop()
 {
-  // Wenn der Knopf gedrückt wird und die Überprüfung noch nicht gestartet ist
-  if (digitalRead(52) == LOW && !start)
-  {
-    // Alle Komponenten und Variablen auf den Start der Überprüfung setzen
-    // ampel.turnOnGreen();
-    servo1.reset();
-    mySwitch.on();
-    anzahlPusten = 0;
-    start = true;
-  }
-
-  // Wenn die Überprüfung noch nicht gestartet ist
-  if (!start)
-  {
-    // Die Ampel auf Rot setzen
-    // ampel.turnOnRed();
-    Timeout = 0;
-  }
-
-  // Wenn während der Überprüfung Airflow erkannt wird
-  if (digitalRead(32) == HIGH && start)
-  {
-    // Erkannte luftströme erhöhen
-    anzahlPusten++;
-
-    // Die Anzahl der Pusten auf dem Oled anzeigen
-    oled1.clear();
-    oled1.drawTip(0, 30, "Anzahl:");
-    String anzahlPustenString = String(anzahlPusten);
-    const char *charPointer = anzahlPustenString.c_str();
-    oled1.drawtexte(75, 40, charPointer);
-
-    // Den Servo um einen Schritt weiter geben
-    servo1.addRotation(1);
-    delay(1000);
-
-    Timeout = 0;
-
-    // Wenn 5 Luftströme erkannt wurden
-    if (anzahlPusten == 5)
-    {
-      // Alles Ausschalten und zurücksetzen
-      mySwitch.off();
-      oled1.clear();
-      {
-        // Nutzer über display mitteilen, das er es geschafft hat
-        oled1.drawTip(0, 30, "Geschafft");
-        servo1.reset();
-      }
-      delay(5000);
-      Timeout = 0;
-      anzahlPusten = 0;
-      start = false;
-    }
-  }
-
-  // Falls für 0,5 Sekunden kein Luftstrom erkannt wird
-  if (Timeout > 10)
-  {
-    // Alles Ausschalten und zurücksetzen
-    mySwitch.off();
-    oled1.clear();
-    // Nutzer über display mitteilen, das er es nicht geschafft hat
-    oled1.drawTip(0, 30, "Nicht Geschafft");
-    servo1.reset();
-    delay(2000);
-    Timeout = 0;
-    anzahlPusten = 0;
-    start = false;
-  }
-
-  // Wenn die Überprüfung gestartet wurde und kein Luftstrom erkannt wird
-  if (start && digitalRead(32) != HIGH)
-  {
-    // Timeout erhöhen
-    Timeout++;
-    delay(50);
-  }
+  checkLoop();
 }
